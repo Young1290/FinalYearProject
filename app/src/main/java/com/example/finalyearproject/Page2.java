5ef135a3ef01e542c13fed1740d94544815cca8c
wentiiiii <jessie.200004@hotmail.com>
2022-07-19 00:56:25 +0800

    UI design
    page 1 to 3 done, login done

--- app/src/main/java/com/example/finalyearproject/Page2.java
+++ app/src/main/java/com/example/finalyearproject/Page2.java
@@ -6,16 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Page2 extends AppCompatActivity {
 
-    private Button button;
+    private ImageView button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
 
-        button = (Button) findViewById(R.id.button3);
+        button = (ImageView) findViewById(R.id.imageView5);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
