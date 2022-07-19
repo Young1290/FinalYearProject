95a9930ceba72dbd407e5bcce7c5149995c7da16
Young1290 <54853532+Young1290@users.noreply.github.com>
2022-07-19 16:24:34 +0800

    Initial commit

--- app/src/main/java/com/example/finalyearproject/Bpm.java
+++ app/src/main/java/com/example/finalyearproject/Bpm.java
@@ -1,13 +1,22 @@
 package com.example.finalyearproject;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 import com.jjoe64.graphview.series.DataPoint;
@@ -15,48 +24,72 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class Bpm extends AppCompatActivity {
 
-    GraphView graphView;
-    @Override
+    private GraphView graphView;
+    private FirebaseDatabase firebaseDatabase;
+    private FirebaseAuth firebaseAuth;
+    private double temp1, temp2, temp3, temp4, temp5;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bpm);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference(firebaseAuth.getUid()).child("pet").child("condition").
+                child("bpm");
 
         graphView = findViewById(R.id.idGraphView);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[]{
+
+        if (firebaseAuth.getCurrentUser() == null) {
+            finish();
+            startActivity(new Intent(getApplicationContext(), Register.class));
+        }
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+
+                LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[]{
+                        // on below line we are adding
+                        // each point on our x and y axis.d
+                        new DataPoint(1, 91),
+                        new DataPoint(2, 92),
+                        new DataPoint(3, 95),
+                        new DataPoint(4, 93),
+                        new DataPoint(5, 96),
+                });
+
+                // after adding data to our line graph series.
+                // on below line we are setting
+                // title for our graph view.
+                graphView.setTitle("BPM Graph View");
+
+                // on below line we are setting
+                // text color to our graph view.
+                graphView.setTitleColor(R.color.purple_200);
+
+                // on below line we are setting
+                // our title text size.
+                graphView.setTitleTextSize(18);
+
                 // on below line we are adding
-                // each point on our x and y axis.
-                new DataPoint(0, 1),
-                new DataPoint(1, 3),
-                new DataPoint(2, 4),
-                new DataPoint(3, 9),
-                new DataPoint(4, 6),
+                // data series to our graph view.
+                graphView.addSeries(series);
+                StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graphView);
+                staticLabelsFormatter.setHorizontalLabels(new String[] {"0", "1", "2", "3", "4", "5","6"});
+                staticLabelsFormatter.setVerticalLabels(new String[] {"80", "90", "100", "110", "120"});
+                graphView.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(Bpm.this, databaseError.getCode(), Toast.LENGTH_SHORT).show();
+            }
         });
-
-        // after adding data to our line graph series.
-        // on below line we are setting
-        // title for our graph view.
-        graphView.setTitle("BPM Graph View");
-
-        // on below line we are setting
-        // text color to our graph view.
-        graphView.setTitleColor(R.color.purple_200);
-
-        // on below line we are setting
-        // our title text size.
-        graphView.setTitleTextSize(18);
-
-        // on below line we are adding
-        // data series to our graph view.
-        graphView.addSeries(series);
-        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graphView);
-        staticLabelsFormatter.setHorizontalLabels(new String[] {"5 days ago", " ", "3 days ago", " ", "today"});
-        staticLabelsFormatter.setVerticalLabels(new String[] {"70", "80", "90", "100", "110"});
-        graphView.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
-
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
         // Set Home selected
-        bottomNavigationView.setSelectedItemId(R.id.nav_person);
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
 
         // Perform item selected listener
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -69,7 +102,7 @@ public class Bpm extends AppCompatActivity {
                         overridePendingTransition(0, 0);
                         return true;
                     case R.id.nav_track:
-                        startActivity(new Intent(getApplicationContext(), HomeFragment.class));
+                        startActivity(new Intent(getApplicationContext(), TrackFragment.class));
                         overridePendingTransition(0, 0);
                         return true;
                     case R.id.nav_person:
@@ -80,6 +113,7 @@ public class Bpm extends AppCompatActivity {
                 return false;
             }
         });
+
     }
 
 }
